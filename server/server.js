bf63691787e39daa8cf284e362bab98612e275fa
Yahelmalka <yahelmalka28@gmail.com>
2026-01-11 19:32:08 +0200

    Uncomment server code to enable functionality

--- server/server.js
+++ server/server.js
@@ -4,21 +4,21 @@
 // This file is only needed if you want to run the server locally or update it
 // The frontend will work fine without this file as long as the server is deployed
 
-//const express = require("express");
-//const cors = require("cors");
+const express = require("express");
+const cors = require("cors");
 
-//const app = express();
-//app.use(cors({ origin: "*" }));
+const app = express();
+app.use(cors({ origin: "*" }));
 
-// app.get("/rates", (req, res) => {
-//   res.json({ USD: 1, GBP: 0.6, EURO: 0.7, ILS: 3.4 });
-// });
+ app.get("/rates", (req, res) => {
+   res.json({ USD: 1, GBP: 0.6, EURO: 0.7, ILS: 3.4 });
+});
 
-// app.get("/", (req, res) => {
-//   res.send("Exchange Rates API running. Use GET /rates");
-// });
+ app.get("/", (req, res) => {
+   res.send("Exchange Rates API running. Use GET /rates");
+ });
 
-//const PORT = process.env.PORT || 3000;
-//app.listen(PORT, () => {
-//console.log(`Server running on port ${PORT}`);
-//});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
